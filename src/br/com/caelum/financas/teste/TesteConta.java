cd54be822a920f6efee0c9a5d4fca00bb1dd2505
unknown <christian.grossi@outlook.com>
2018-03-07 22:26:06 -0300

    first commit

---
+++ src/br/com/caelum/financas/teste/TesteConta.java
@@ -0,0 +1,17 @@
+package br.com.caelum.financas.teste;
+
+import br.com.caelum.financas.modelo.Conta;
+
+public class TesteConta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Conta conta = new Conta();
+		conta.setTitular("Ronaldo");
+		conta.setAgencia("1530");
+		conta.setBanco("Itaú");
+		conta.setNumero("05678");
+		
+	}
+
+}
