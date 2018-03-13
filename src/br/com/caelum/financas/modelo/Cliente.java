deec294e7f6da57bf243a442fa7bdebaf48f9735
unknown <christian.grossi@outlook.com>
2018-03-13 12:54:59 -0300

    JPQL

--- src/br/com/caelum/financas/modelo/Cliente.java
+++ src/br/com/caelum/financas/modelo/Cliente.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -13,9 +14,10 @@ public class Cliente {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer Id;
 	private String nome;
-	private String proficao;
+	private String profissao;
 	private String endereco;
 	
+	@JoinColumn(unique=true)
 	@OneToOne
 	private Conta conta;
 	
@@ -31,17 +33,23 @@ public class Cliente {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getProficao() {
-		return proficao;
+	public String getProfissao() {
+		return profissao;
 	}
-	public void setProficao(String proficao) {
-		this.proficao = proficao;
+	public void setProfissao(String proficao) {
+		this.profissao = proficao;
 	}
 	public String getEndereco() {
 		return endereco;
 	}
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
+	}
+	public Conta getConta() {
+		return conta;
+	}
+	public void setConta(Conta conta) {
+		this.conta = conta;
 	}
 
 }
--- src/br/com/caelum/financas/teste/TestaContaCliente.java
+++ src/br/com/caelum/financas/teste/TestaContaCliente.java
@@ -2,30 +2,31 @@ package br.com.caelum.financas.teste;
 
 import javax.persistence.EntityManager;
 
+import br.com.caelum.financas.modelo.Cliente;
 import br.com.caelum.financas.modelo.Conta;
 import br.com.caelum.financas.util.JPAUtil;
 
-public class TesteConta {
+public class TestaContaCliente {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Cliente cliente = new Cliente();
+		cliente.setNome("Josiano");
+		cliente.setEndereco("Rua Beutrano 12");
+		cliente.setProfissao("Professor");
+		
 		Conta conta = new Conta();
-		//conta.setId(1);
-		conta.setTitular("Robson");
-		conta.setAgencia("1526");
-		conta.setBanco("Bradesco");
-		conta.setNumero("03384");		
-	
+		conta.setId(2);
+		conta.setTitular(cliente.getNome());
+		cliente.setConta(conta);
+		
 		EntityManager em = new JPAUtil().getEntityManager();
 		em.getTransaction().begin();
-		//conta = em.find(Conta.class, 1);
-		em.persist(conta);
-		//conta.setBanco("Bradesco");
+		
+		em.persist(cliente);
 		em.getTransaction().commit();
 		em.close();
-		
-	
+
 	}
-	
 
 }
