a257dc7ea55d03e0a25f6caa519889abdedf6e93
unknown <christian.grossi@outlook.com>
2018-03-24 22:40:00 -0300

    mapped by

--- src/br/com/caelum/financas/modelo/Movimentacao.java
+++ src/br/com/caelum/financas/modelo/Movimentacao.java
@@ -14,6 +14,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import br.com.caelum.financas.modelo.Conta;
 
 @Entity
 public class Movimentacao {
---
+++ src/br/com/caelum/financas/teste/TesteTodasMovimentacoesDasContas.java
@@ -0,0 +1,34 @@
+package br.com.caelum.financas.teste;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public abstract class TesteTodasMovimentacoesDasContas {
+
+	public static void main(String[] args) {
+		
+		EntityManager em = new JPAUtil().getEntityManager();
+		em.getTransaction().begin();
+		
+		String jpql = "select c from Conta c join fetch c.movimentacoes";
+		Query query = em.createQuery(jpql);
+		
+		List<Conta> todasAsContas = query.getResultList();
+		for (Conta conta : todasAsContas) {
+			System.out.println(conta.getTitular());
+			System.out.println("Moviemntacoes");
+			System.out.println(conta.getMovimentacoes());
+		}
+		
+		em.getTransaction().commit();
+		em.close();
+
+	}
+
+}
