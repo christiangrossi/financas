71a532885e71e219cacf7a6449ad570d8ca1d2dd
unknown <christian.grossi@outlook.com>
2018-03-30 11:27:48 -0300

    Dao

--- src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
+++ src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
+import br.com.caelum.financas.dao.MovimentacaoDao;
 import br.com.caelum.financas.modelo.Conta;
 import br.com.caelum.financas.modelo.TipoMovimentacao;
 import br.com.caelum.financas.util.JPAUtil;
@@ -20,22 +21,14 @@ public class TesteFuncoesJPQL {
 				Conta conta = new Conta();
 				conta.setId(2);
 
-				String jpql = "select distinct avg(m.valor) from Movimentacao m where m.conta = :pConta "
-						+ "and m.tipo = :pTipo"
-						+ " group by m.data";
-
-				TypedQuery<Double> query = em.createQuery(jpql, Double.class);
-				query.setParameter("pConta", conta);
-				query.setParameter("pTipo", TipoMovimentacao.SAIDA);
-
-				List<Double> medias =  query.getResultList();
+				MovimentacaoDao dao = new MovimentacaoDao(em);
+				
+				List<Double> medias =  dao .getMediasPorDiaETipo(TipoMovimentacao.SAIDA, conta);
 				for (Double media : medias) {
 					System.out.println("a media: " + media);
 				}
 				
 				em.getTransaction().commit();
-				
-				
 				em.close();
 		
 	}
